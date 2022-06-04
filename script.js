7ffaddeaf553730eaa7e8659e1dc7d06111f57ea
Jillian Wells <102785081+CapatlCity@users.noreply.github.com>
2022-06-03 23:18:52 -0400

    tablefunctionalityadd060322

--- script.js
+++ script.js
@@ -11,10 +11,43 @@ inputForm.addEventListener("submit", (event) => {
   console.log(firstName);
   console.log(lastName);
   console.log(grade);
+
+  var tableInput = document.createElement("tr"); // table row created in js via document query
+  var outputTable = document.getElementById("gradesTable"); //this is example of a DOM NODE!
+  outputTable.appendChild(tableInput);
 });
 
-
 // TO DO
-// have form populate input fromm form into table
+// PUT OUTPUT IN THE TABLE
+// have form populate input from form into table
 // decide if we want colors to change as grade and names calculated
 // create loop to calculate grade and determine what color will output
+
+/* let correct_button = document.querySelector("#correct"); //change color of the 4th answer button from white to green when clicked.
+    correct_button.addEventListener('click', function() {
+        correct_button.style.backgroundColor = "green";
+        document.querySelector('#feedback1').innerHTML = 'Correct!';
+
+});
+
+USE TRIVIA LAB CODE AS A GUIDE OF SORTS -
+ SEE IF THE DOCUMENT.QUERYSELECTOR FOR #FEEEDBACK CAN WORK 
+IF  <P> IS PLACED INSIDE OF A TABLE CELL
+let wrong_button = document.querySelectorAll(".wrong");
+    //change color of the 1st through 3rd buttons from white to red when clicked.
+    for (let i = 0; i < wrong_button.length; i++)
+    {
+    wrong_button[i].addEventListener('click', function() {
+        wrong_button[i].style.backgroundColor = "red";
+        document.querySelector('#feedback1').innerHTML = 'Wrong!';
+
+    });
+
+    }
+
+// Part 2: add shortanswer form with text input and "check answer" submit button to HTML.
+//remember DIV id is "shortanswer" and is linked to CSS styling
+
+
+   
+    });    */
