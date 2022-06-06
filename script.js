45f811f47ea18fb83fb9874fa62f5f69cd607357
Jillian Wells <102785081+CapatlCity@users.noreply.github.com>
2022-06-05 20:56:23 -0400

    conditionalloopaddattempt060522

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var gradesTable = document.getElementById("gradesTable");
 var inputForm = document.getElementById("inputForm");
 
-// build form that functions well enough to take responses
 // override default browser reset after "submit"
 inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,32 +11,48 @@ inputForm.addEventListener("submit", (event) => {
   console.log(lastName);
   console.log(grade);
 
-// create new var, calculate grades
+  // create new var, calculate grades
 
+  // create conditional if-else if-else loop for converting numerical value to ABCDF
 
-  var tableInput = document.createElement("tr"); // table row created in js via document query
-  
+  var tableInput = document.createElement("td"); // table row created in js via document query
+
+  // have table display first name, last name from form "as is"
+  //-- populate firstname and lastname input from form field into table via a function
+
   var tableFirstName = document.createElement("td");
-      tableFirstName.innerHTML = firstName; 
-      tableFirstName.appendChild(tableInput); // attenpt to append td nodes to tr node per line 33
+  tableFirstName.innerHTML = firstName;
+  tableFirstName.appendChild(tableInput); // attenpt to append td nodes to tr node per line 33
 
   var tableLastName = document.createElement("td");
-      tableLastName.innerHTML = lastName;
-      tableLastName.appendChild(tableInput); // attenpt to append td nodes to tr node per line 33
+  tableLastName.innerHTML = lastName;
+  tableLastName.appendChild(tableInput); // attenpt to append td nodes to tr node per line 33
 
   var tableGrade = document.createElement("td"); // create GH issue for converting numeric data into ABCDF
-      tableGrade.innerHTML = grade;
-      tableGrade.appendChild(tableInput);  // attenpt to append td nodes to tr node per line 33
-      
+  tableGrade.innerHTML = grade;
+  tableGrade.appendChild(tableInput); // attenpt to append td nodes to tr node per line 33
+
   var outputTable = document.getElementById("gradesTable"); //this is example of a DOM NODE!
-      outputTable.appendChild(tableInput);
+  outputTable.appendChild(tableInput);
 });
+//have table display numeric grade converted into ABCD or F
 
+//have table display a color based on alphabetical grade output using a boolean outputting green yellow orange red purple for ABCDF respectively
 
+const gradeInput = document.getElementById("grade").value;
+if (gradeInput > 90) {
+  TextDecoder.innerHTML("grade A");
+} else if (gradeInput < 90 || gradeInput >= 80) {
+  console.log("grade B)");
+} else if (gradeInput < 80 || gradeInput >= 70) {
+  console.log("grade C");
+} else if (gradeInput < 70 || gradeInput >= 60) {
+  console.log("grade D");
+} else if (gradeInput < 60) {
+  console.log("grade F");
+} else {
+  console.log("try again");
+}
 
-
-
-
-
-
-
+//document.querySelector(tbd).addEventListener('submit', functionnametbd)
+//functiontbd
