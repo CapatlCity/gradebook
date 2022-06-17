a30b9d77b9d839293f6b153f8b8af79039096cdb
Jillian Wells <102785081+CapatlCity@users.noreply.github.com>
2022-06-17 13:14:27 -0400

    Moved files to gradebook

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var gradesTable = document.getElementById("gradesTable");
+
 var inputForm = document.getElementById("inputForm");
 
 inputForm.addEventListener("submit", (event) => {
   event.preventDefault("submit"); // override default browser reset after "submit"
+
   var firstName = document.getElementById("fname").value;
+
   var lastName = document.getElementById("lname").value;
+
   var grade = document.getElementById("grade").value;
   console.log(firstName);
   console.log(lastName);
@@ -12,41 +16,48 @@ inputForm.addEventListener("submit", (event) => {
 });
 
 var gradeArray = [
-  { firstName: "Dan", lastName: "Rosenbaum", gradeOutput: 95 },
-  { firstName: "Deb", lastName: "ONair", gradeOutput: 95 },
-  { firstName: "Jane", lastName: "Doe", gradeOutput: 95 },
+  { firstName: "Arthur", lastName: "Aardvark", gradeOutput: 85 },
+  { firstName: "Billy", lastName: "Billington", gradeOutput: 75 },
+  { firstName: "Chuckie", lastName: "Cheese", gradeOutput: 65 },
+  { firstName: "Deb", lastName: "ONair", gradeOutput: 55 },
 ];
 
+// step 1: create new row//
 function renderTable() {
   gradeArray.forEach((grade) => {
     //generate a code block for each grade
-    var newRow = document.createElement("tr");
+    var newRow = document.createElement("tr"); // review use of ` and practice functions! //
+    //this function is  is what creates new table rows//
     newRow.classList.add(rowColor(grade.gradeOutput));
-    newRow.innerHTML = `<td>${grade.firstName}</td>
+
+    newRow.innerHTML = `<td>${grade.firstName}</td>   
+    
     <td>${grade.lastName}</td>
+    
     <td>${grade.gradeOutput}</td>`;
-    //append it to the gradesTable
+
+    // must create indvidually animating nodes for each data cell in the row that is intended to appear at the bottom
+    // of the gradesTable ie appendages //
     gradesTable.appendChild(newRow);
   });
 }
+
+//step 2: calculate input and generate a color in the newly animated row//
 
 function rowColor(grade) {
   if (grade >= 90) {
     return "green";
   } else if (grade >= 80) {
     return "yellow";
-  }
+  } else if (grade >= 70) {
+    return "orange";
+  } else if (grade >= 60) {
+    return "red";
+  } else grade < 60;
+  return "brown";
 }
 
 renderTable();
-
-// TO DO - have table display numeric grade converted into ABCD or F
-
-// TO DO - have table display a color based on alphabetical grade output using a boolean outputting green yellow orange red purple for ABCDF respectively
-// create GH issue for converting numeric data into ABCDF
-
-//THINGS TO TRY
-// attenpt to append td nodes to tr node per line 33
 
 const gradeInput = document.getElementById("grade").value;
 
